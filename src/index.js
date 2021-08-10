079349a77a3be011ea9a7684d8c40a9d2815851e
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-08-10 17:21:59 -0400

    about to work code org

--- src/index.js
+++ src/index.js
@@ -88,6 +88,69 @@ function patchUpVote(event, downvoteEle, decrease=false) {
 
 }
 
+function patchDownVote(event, upvoteEle, increase=false) {
+    const num_votes_ele = event.target.previousElementSibling
+
+    let num_votes = parseInt(num_votes_ele.textContent)
+
+   increase ? num_votes += 2 : num_votes += 1
+
+    num_votes_ele.innerHTML = ""
+    num_votes_ele.innerText = num_votes
+
+    event.target.classList.remove("bi-caret-down-square-fill")
+    event.target.classList.add("bi-caret-down-square")
+    event.target.classList.remove("active")
+    event.target.style.color = "#adb5bd"
+
+    if (increase) {
+
+
+        upvoteEle.classList.remove("bi-caret-down-square-fill")
+        upvoteEle.classList.add("bi-caret-down-square")
+        upvoteEle.classList.remove("active")
+        upvoteEle.style.color = "#adb5bd"
+
+        event.target.classList.remove("bi-caret-up-square")
+        event.target.classList.add("bi-caret-up-square-fill")
+        event.target.classList.add("active")
+        event.target.style.color = "#0d6efd" 
+        
+
+    }
+    
+    const box = event.target.closest("div.comment-box")
+
+    const course_id = box.dataset.id 
+
+    updateVoteScore(course_id, num_votes)
+
+
+    let upvote;
+
+    if (increase) {
+        upvote = false
+    } else {
+        upvote = null
+    }
+
+
+    const voteObj = {
+        upvote
+    }
+
+    fetch(`http://localhost:3000/votes/${event.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "Application/json",
+          "Accept": "Application/json"
+        },
+        body: JSON.stringify(voteObj)
+      })
+
+
+}
+
 function createVote(event, downvote=false) {
     let num_votes_ele;
 
@@ -1015,7 +1078,19 @@ main_body.addEventListener('click', event => {
                 else if (event.target.matches('i.downvote')) {
                     console.log("clicked")
 
+                    const up = event.target.previousElementSibling.previousElementSibling
+
+                    if (event.target.classList.contains('active')) {
+                        patchDownVote(event, up)
+
+                    } 
+                    else if (up.classList.contains('active')) {
+                        patchDownVote(event, up, true)
+
+                    } else {
                     createVote(event, true)
+                    }
+
 
 
 
