24ed686703565ffc7a9b3812b63b0ff06515135a
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-08-11 01:01:19 -0400

    fixed downvote patching

--- src/vote.js
+++ src/vote.js
@@ -118,7 +118,7 @@ function patchUpVote(event, downvoteEle, student, decrease=false) {
 
 }
 
-function patchDownVote(event, upvoteEle, increase=false) {
+function patchDownVote(event, upvoteEle, student, increase=false) {
     const num_votes_ele = event.target.previousElementSibling
 
     let num_votes = parseInt(num_votes_ele.textContent)
@@ -169,9 +169,38 @@ function patchDownVote(event, upvoteEle, increase=false) {
         upvote
     }
 
+    const allVoteEles = box.getElementsByTagName('li')
+
+    console.log(allVoteEles)
+
+    // const voteEle = allVoteEles.find(voteEle => {
+    //     voteEle.innerText === student.id
+    // })
+
+    let student_ids = []
+    let vote_ids = []
     
 
-    fetch(`http://localhost:3000/votes/${event.target.dataset.id}`, {
+
+    for (let i = 0; i < allVoteEles.length; i++) {
+        // console.log(allVoteEles[i].textContent)
+        // console.log(allVoteEles[i].dataset.id)
+        // if (allVoteEles[i].textContent === student.id) {
+            
+        // }
+       student_ids.push(parseInt(allVoteEles[i].textContent))
+       vote_ids.push(allVoteEles[i].dataset.id)
+        // console.log(allVoteEles[i].dataset.id)
+
+    }
+
+    // console.log(student_ids)
+
+    const vote_id_index = student_ids.indexOf(parseInt(student.id))
+
+    
+
+    fetch(`http://localhost:3000/votes/${vote_ids[vote_id_index]}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "Application/json",
